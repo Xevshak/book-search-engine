e1fce8a0fa198b8291abc2da4fb9fcec2e881c3a
Xevshak <keshavsternkrishnan@gmail.com>
2021-12-17 15:39:45 -0500

    hardcoded

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect('mongodb+srv://loopers:Loop@cluster0.nkcnk.mongodb.net/workoutdb?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
